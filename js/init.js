771130cd9b73118f4077c8014b382c2bea02f474
Tyler Woods <tyler.woods66@gmail.com>
2016-07-25 11:44:56 -0500

    side nav show on large

--- js/init.js
+++ js/init.js
@@ -1,7 +1,13 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $('.button-collapse').sideNav();
 
-  }); // end of document ready
+        $('.button-collapse').sideNav({
+            menuWidth: 300, // Default is 240
+            edge: 'right', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        });
+
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
