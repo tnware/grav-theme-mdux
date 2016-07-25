e938b2657aa404bb65860ef70f24cfbc9f074cc8
Tyler Woods <tyler.woods66@gmail.com>
2016-07-25 11:35:05 -0500

    revert

--- js/init.js
+++ js/init.js
@@ -1,16 +1,7 @@
-(function($) {
-    $(function() {
-        $('.carousel').carousel();
-        // Next slide
-        $('.carousel').carousel('next');
-        $('.carousel').carousel('next', 3); // Move next n times.
-        // Previous slide
-        $('.carousel').carousel('prev');
-        $('.carousel').carousel('prev', 4); // Move prev n times.
-        // Set to nth slide
-        $('.carousel').carousel('set', 4);
+(function($){
+  $(function(){
 
-        $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav();
 
-    }); // end of document ready
+  }); // end of document ready
 })(jQuery); // end of jQuery name space
