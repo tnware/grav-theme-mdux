59654a16842bbbfb2b41abfa9083e21cb6f4d306
Tyler Woods <tyler.woods66@gmail.com>
2016-07-25 11:45:47 -0500

    remove right align

--- js/init.js
+++ js/init.js
@@ -4,7 +4,6 @@
 
         $('.button-collapse').sideNav({
             menuWidth: 300, // Default is 240
-            edge: 'right', // Choose the horizontal origin
             closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
         });
 
