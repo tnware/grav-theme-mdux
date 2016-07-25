57d13164ee36f4ac0b099c7ec2c16dfeedfda28e
Tyler Woods <tyler.woods66@gmail.com>
2016-07-25 11:22:01 -0500

    initialize carousel

--- js/init.js
+++ js/init.js
@@ -1,6 +1,18 @@
 (function($){
   $(function(){
 
+        $(document).ready(function(){
+          $('.carousel').carousel();
+        });
+        // Next slide
+$('.carousel').carousel('next');
+$('.carousel').carousel('next', 3); // Move next n times.
+// Previous slide
+$('.carousel').carousel('prev');
+$('.carousel').carousel('prev', 4); // Move prev n times.
+// Set to nth slide
+$('.carousel').carousel('set', 4);
+  
     $('.button-collapse').sideNav();
 
   }); // end of document ready
