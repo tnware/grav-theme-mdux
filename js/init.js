2a189934c19f51e614185cbcb2d42a537941994f
Tyler Woods <tyler.woods66@gmail.com>
2016-07-25 19:09:05 -0500

    carousel function

--- js/init.js
+++ js/init.js
@@ -7,6 +7,9 @@
             closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
         });
 
-
+        $(document).ready(function(){
+          $('.carousel').carousel();
+        });
+            
     }); // end of document ready
 })(jQuery); // end of jQuery name space
