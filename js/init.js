82bf60f4770c683cde7c0a10f0c0708f03601463
Tyler Woods <tyler.woods66@gmail.com>
2016-07-26 15:05:43 -0500

    0.9.5 base

--- js/init.js
+++ js/init.js
@@ -38,40 +38,50 @@
         }
       });
     });
-          // Plugin initialization
-              $('.carousel.carousel-slider').carousel({full_width: true});
-              $('.carousel').carousel();
-              $('.slider').slider({full_width: true});
-              $('.parallax').parallax();
-              $('.modal-trigger').leanModal();
-              $('.scrollspy').scrollSpy();
-              $('.button-collapse').sideNav({'edge': 'left'});
-              $('.datepicker').pickadate({selectYears: 20});
-              $('select').not('.disabled').material_select();
-              $('input.autocomplete').autocomplete({
-                data: {"Apple": null, "Microsoft": null, "Google": 'http://placehold.it/250x250'}
-              });
-              $('.chips-initial').material_chip({
-                  readOnly: true,
-                  data: [{
-                    tag: 'Apple',
-                  }, {
-                    tag: 'Microsoft',
-                  }, {
-                    tag: 'Google',
-                  }]
-                });
 
-                $('.chips-placeholder').material_chip({
-                  placeholder: 'Enter a tag',
-                  secondaryPlaceholder: '+Tag',
-                });
+    // Plugin initialization
+    $('.carousel.carousel-slider').carousel({
+        full_width: true
+    });
+    $('.carousel').carousel();
+    $('.slider').slider({
+        full_width: true
+    });
+    $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+    $('.scrollspy').scrollSpy();
+    $('.button-collapse').sideNav({
+        'edge': 'left'
+    });
+    $('.datepicker').pickadate({
+        selectYears: 20
+    });
+    $('select').not('.disabled').material_select();
+    $('input.autocomplete').autocomplete({
+        data: {
+            "Apple": null,
+            "Microsoft": null,
+            "Google": 'http://placehold.it/250x250'
+        }
+    });
 
-                $('.chips').material_chip();
-        $('.button-collapse').sideNav({
-            menuWidth: 300, // Default is 240
-            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        });
+    $('.chips-initial').material_chip({
+        readOnly: true,
+        data: [{
+            tag: 'Apple',
+        }, {
+            tag: 'Microsoft',
+        }, {
+            tag: 'Google',
+        }]
+    });
+
+    $('.chips-placeholder').material_chip({
+        placeholder: 'Enter a tag',
+        secondaryPlaceholder: '+Tag',
+    });
+
+    $('.chips').material_chip();
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
