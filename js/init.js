3035bd44c79c481fc9f2e617d465ff10c38989ec
Tyler Woods <tyler.woods66@gmail.com>
2016-07-27 13:08:37 -0500

    remove extra

--- js/init.js
+++ js/init.js
@@ -18,28 +18,6 @@
             });
         }
 
-        // Toggle Flow Text
-        var toggleFlowTextButton = $('#flow-toggle');
-        toggleFlowTextButton.click(function() {
-            $('#flow-text-demo').children('p').each(function() {
-                $(this).toggleClass('flow-text');
-            });
-        });
-
-        //    Toggle Containers on page
-        var toggleContainersButton = $('#container-toggle-button');
-        toggleContainersButton.click(function() {
-            $('body .browser-window .container, .had-container').each(function() {
-                $(this).toggleClass('had-container');
-                $(this).toggleClass('container');
-                if ($(this).hasClass('container')) {
-                    toggleContainersButton.text("Turn off Containers");
-                } else {
-                    toggleContainersButton.text("Turn on Containers");
-                }
-            });
-        });
-
         // Plugin initialization
         $('.carousel.carousel-slider').carousel({
             full_width: true
